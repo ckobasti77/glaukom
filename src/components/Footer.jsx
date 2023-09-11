c98d11e933fb74fcaf0361be77084ea8b73a89a6
Jovan Milojevic <jovanm028@gmail.com>
2023-09-11 17:01:22 +0200

    comm

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <div className='bg-fourth text-secondary flex justify-center md:justify-between px-24 py-2 flex-wrap h-auto items-center rounded-t-2xl lg:rounded-t-full'>
         <span>Poliklinika Jatros. © 2023.</span>
         <span className='flex flex-col md:inline '>
-          <a href="tel:+381 64 180 66 65" className='underline mx-4'>+381 64 180 66 65</a>
+          <a href="tel:+381 64 180 66 56" className='underline mx-4'>+381 64 180 66 56</a>
           <a href="mailto:jatros.ord@gmail.com" className='underline mx-4'>jatros.ord@gmail.com</a>
         </span>
     </div>
